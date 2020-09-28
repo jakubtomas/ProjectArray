15aee2197d03215de8db73bc9597539f7e2e3fdd
tomasjakub <tomasjakub21@gmail.com>
2020-09-28 20:06:35 +0200

    "done functions checkCountAllValues "

--- src/sk/itsovy/sudoka/Game.java
+++ src/sk/itsovy/sudoka/Game.java
@@ -1,4 +1,27 @@
 package sk.itsovy.sudoka;
 
 public class Game {
+    private Board board;
+    private boolean state;
+
+    public Game(Board board) {
+        this.board = board;
+        this.state = false;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
 }
--- src/sk/itsovy/sudoka/controller/SudokuSolver.java
+++ src/sk/itsovy/sudoka/controller/SudokuSolver.java
@@ -53,14 +53,17 @@ public class SudokuSolver {
         //System.out.println();
         // zistit poziciu
         // from class Tile , reduce values according the values in array
-        int count = 0;
-        while (count<15) { // todo create function which will be control count number horizontal, vertical , squat and all number in Board
-            count++;
+        boolean includeBoardZero = true;
+        while (includeBoardZero ) {
+
             reduceAvailableValueInRow();
             reduceAvailableValueInCol();
             reduceAvailableValuesInSqr();
-        }
 
+            //after every reduce values check that we have empty space in some place
+            includeBoardZero = includeBoardSpace();
+        }
+        checkCountAllValues();
         //Set<Integer> available
         showPossibleValuesForTile();
     }
@@ -68,31 +71,18 @@ public class SudokuSolver {
     private void reduceAvailableValueInRow() { // riadky hore dole
         // board.getTiles()[i][j];
 
-       /* System.out.println(board.getTiles()[5][0].getAvailable() + " Possible value");
-        System.out.println(board.getTiles()[5][0].getValue() + " Value");
-
-        System.out.println(board.getTiles()[1][0].getAvailable() + " Possible values");
-        System.out.println(board.getTiles()[1][0].getValue() + " Value");
-
-        System.out.println("------------------");
-
-        board.getTiles()[1][0].getAvailable().remove(4);
-        System.out.println(board.getTiles()[1][0].getAvailable() + " Possible value after delete value");
-*/
-        System.out.println("============================================================");
-        System.out.println("============================================================");
         for (int i = 0; i < 9; i++) { // riadok row
             for (int j = 0; j < 9; j++) {// stlpec column
 
                 System.out.println(board.getTiles()[i][j].getValue() + " <.......Work fot tile");
                 if (board.getTiles()[i][j].getValue() == 0) {
-                    for (int k = 0; k <9 ; k++) {  // cely riadok z lava do prava meni sa len pozicia stlpca
+                    for (int k = 0; k < 9; k++) {  // cely riadok z lava do prava meni sa len pozicia stlpca
                         //board.getTiles()[i][k].getValue();
 
                         // if available include number from this row delete this
 
-                        if (board.getTiles()[i][k].getValue() >0) { // delete values only bigger than zero
-                            System.out.println(" " +board.getTiles()[i][k].getValue() + " delete for " + board.getTiles()[i][j].getValue()+ " " +" [" +i+"] [" +j+ "]");
+                        if (board.getTiles()[i][k].getValue() > 0) { // delete values only bigger than zero
+                            System.out.println(" " + board.getTiles()[i][k].getValue() + " delete for " + board.getTiles()[i][j].getValue() + " " + " [" + i + "] [" + j + "]");
                             System.out.println("===" + k);
                             board.getTiles()[i][j].getAvailable().remove(board.getTiles()[i][k].getValue());
                         }
@@ -101,9 +91,6 @@ public class SudokuSolver {
                         }
                         // podmienka ked Tile ma iba jednu available tak pridat  setValue
 
-                    /*if (board.getTiles()[i][j].getAvailable().size() ==1) {
-                        board.getTiles()[i][j].setValue();
-                    }*/
                     }
                     System.out.println("--------------");
                 }
@@ -120,20 +107,19 @@ public class SudokuSolver {
 
     private void reduceAvailableValueInCol() { // my own
         System.out.println(".------------------------ reduce Available In col");
-        System.out.println(".------------------------ reduce Available In col");
-        System.out.println(".------------------------ reduce Available In col");
+
         for (int i = 0; i < 9; i++) { // riadok row
             for (int j = 0; j < 9; j++) {// stlpec column
 
                 System.out.println(board.getTiles()[i][j].getValue() + " <.......Work fot tile");
                 if (board.getTiles()[i][j].getValue() == 0) {
-                    for (int k = 0; k <9 ; k++) {  // cely riadok z lava do prava meni sa len pozicia stlpca
+                    for (int k = 0; k < 9; k++) {  // cely riadok z lava do prava meni sa len pozicia stlpca
                         //board.getTiles()[i][k].getValue();
 
                         // if available include number from this row delete this
 
-                        if (board.getTiles()[k][j].getValue() >0) { // delete values only bigger than zero
-                            System.out.println(" " +board.getTiles()[k][j].getValue() + " delete for " + board.getTiles()[i][j].getValue()+ " " +" [" +i+"] [" +j+ "]");
+                        if (board.getTiles()[k][j].getValue() > 0) { // delete values only bigger than zero
+                            System.out.println(" " + board.getTiles()[k][j].getValue() + " delete for " + board.getTiles()[i][j].getValue() + " " + " [" + i + "] [" + j + "]");
 
                             board.getTiles()[i][j].getAvailable().remove(board.getTiles()[k][j].getValue());
                         }
@@ -142,9 +128,7 @@ public class SudokuSolver {
                         }
                         // podmienka ked Tile ma iba jednu available tak pridat  setValue
 
-                    /*if (board.getTiles()[i][j].getAvailable().size() ==1) {
-                        board.getTiles()[i][j].setValue();
-                    }*/
+
                     }
                     System.out.println("--------------");
                 }
@@ -155,28 +139,27 @@ public class SudokuSolver {
     }
 
 
-
     private void reduceAvailableValuesInSqr() { // copy from Miroslav Jackanin okay but maybe I will create my own algorithm
-        int k=0, l=0, m=0, n=0;
-        for (int i=0; i<board.getTiles().length; i+=3){
-            for (int j=0; j<board.getTiles().length; j+=3){
-                for (k+=i; k<i+3; k++) {
-                    for (l+=j; l<j+3; l++) {
+        int k = 0, l = 0, m = 0, n = 0;
+        for (int i = 0; i < board.getTiles().length; i += 3) {
+            for (int j = 0; j < board.getTiles().length; j += 3) {
+                for (k += i; k < i + 3; k++) {
+                    for (l += j; l < j + 3; l++) {
                         if (board.getTiles()[k][l].getAvailable() == null) {
-                            for (m+=i; m<i+3; m++) {
-                                for (n+=j; n<j+3; n++) {
+                            for (m += i; m < i + 3; m++) {
+                                for (n += j; n < j + 3; n++) {
                                     if (board.getTiles()[m][n].getAvailable() != null) {
                                         board.getTiles()[m][n].remove(board.getTiles()[k][l].getValue());
                                     }
                                 }
-                                n=0;
+                                n = 0;
                             }
-                            m=0;
+                            m = 0;
                         }
                     }
-                    l=0;
+                    l = 0;
                 }
-                k=0;
+                k = 0;
             }
         }
     }
@@ -184,11 +167,10 @@ public class SudokuSolver {
 
     private void showPossibleValuesForTile() {
         System.out.println("============================");
-        System.out.println("============================");
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (board.getTiles()[i][j].getAvailable() !=null) {
-                    System.out.println(board.getTiles()[i][j].getAvailable() + "  " + i+ "|" + j );
+                if (board.getTiles()[i][j].getAvailable() != null) {
+                    System.out.println(board.getTiles()[i][j].getAvailable() + "  " + i + "|" + j);
 
                 }
             }
@@ -197,5 +179,30 @@ public class SudokuSolver {
         System.out.println("============================");
     }
 
+    private void checkCountAllValues() {
+        int count = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                count = count + board.getTiles()[i][j].getValue();
+            }
+            System.out.println("");
+        }
+
+        System.out.println(" Count is " + count);
+
+        // return count == 405;
+    }
+
+    private boolean includeBoardSpace() {
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board.getTiles()[i][j].getValue() == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 }
